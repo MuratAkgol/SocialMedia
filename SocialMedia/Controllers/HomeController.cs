7aae6697ff54fa07b5a112ef7cdf84fd47c04b27
birkanturan10 <117521602+birkanturan10@users.noreply.github.com>
2023-06-23 14:19:38 +0300

    SocialMedia

---
+++ SocialMedia/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialMedia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Hakkimda()
+        {
+            return View();
+        }
+
+        public IActionResult Iletisim()
+        {
+            return View();
+        }
+
+        public IActionResult KayitOl()
+        {
+            return View();
+        }
+    }
+}
